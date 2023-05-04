41db41f7a67c09a3e31df23b16b2a035b9c7a6c1
Gabriel Galeski <galeski35@gmail.com>
2023-05-04 19:38:20 -0300

    Refatorando testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, screen, within } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from '../App';
 import mockData from '../helper/mockData';
 import AppProvider from '../contexts/AppProvider';
@@ -13,261 +13,137 @@ describe('Testando o App', () => {
         });
       });
 
-  it('Testando a aplicação pelo app', async () => {
+  it('Testando se tudo é renderizado', async () => {
       render(
         <AppProvider>
             <App />
         </AppProvider>
       )
-
-      expect(screen.getByRole('textbox')).toBeInTheDocument()
-      expect(screen.getByRole('spinbutton')).toBeInTheDocument()
-      expect(screen.getByRole('button', {
-        name: 'Filtrar'
-      })).toBeInTheDocument()
-      expect(global.fetch).toBeCalledTimes(1)
+      
+      expect(global.fetch).toBeCalled()
+      expect(global.fetch).toHaveBeenCalledWith('https://swapi.dev/api/planets')
+      expect(screen.getByTestId('column-sort')).toBeInTheDocument()
+      expect(screen.getByTestId('column-sort-input-asc')).toBeInTheDocument()
+      expect(screen.getByTestId('name-filter')).toBeInTheDocument()
+      expect(screen.getByTestId('name-filter')).toBeInTheDocument()
       expect(screen.getByTestId('column-filter')).toBeInTheDocument()
-      expect(screen.getByRole('option', { name: 'maior que' }).selected).toBe(true)
-      expect(screen.getAllByRole('option').length).toBe(13)
-      expect(screen.getAllByRole('columnheader').length).toBe(13)
-
-      const getTatooName = await screen.findByRole('cell', {
-        name: /tatooine/i
-      })
-
-      expect(getTatooName).toBeInTheDocument()
-
-      const getKaminoName = await screen.getByRole('cell', {
-        name: /kamino/i
-      })
-
-      expect(getKaminoName).toBeInTheDocument()
-
-      const inputName = screen.getByRole('textbox');
-
-      userEvent.type(inputName, 'Alderaan')
-
-      expect(getTatooName).not.toBeInTheDocument()
-      expect(screen.getByRole('cell', {
-        name: /alderaan/i
-      })).toBeInTheDocument()
-  })
-
-  it('Testes no componente Filter', async () => {
-    render(
-        <AppProvider>
-            <App />
-        </AppProvider>
-      )
-
-      const getValue = screen.getByRole("spinbutton");
-      const getButton = screen.getByRole('button', {
-        name: 'Filtrar'
-      })
-      const column = screen.getByTestId('column-filter')
-      const comparison = screen.getByTestId('comparison-filter')
-
-      userEvent.selectOptions(column, 'rotation_period')
-      userEvent.selectOptions(comparison, 'igual a')
-      userEvent.type(getValue, '24')
-      userEvent.click(getButton)
-
-      expect(await screen.findByRole('cell', {
-        name: /alderaan/i
-      })).toBeInTheDocument()
-
-      expect(await screen.findByRole('cell', {
-        name: /coruscant/i
-      })).toBeInTheDocument()
-  })
-
-  it('Testando filtros "menor que" ', async () => {
-    render(
-        <AppProvider>
-            <App />
-        </AppProvider>
-      )
-
-      const getValue = screen.getByRole("spinbutton");
-      const getButton = screen.getByRole('button', {
-        name: 'Filtrar'
-      })
-      const column = screen.getByTestId('column-filter')
-      const comparison = screen.getByTestId('comparison-filter')
-
-      userEvent.selectOptions(column, 'rotation_period')
-      userEvent.selectOptions(comparison, 'menor que')
-      userEvent.type(getValue, '20')
-      userEvent.click(getButton)
-
-      expect(await screen.findByRole('cell', {
-        name: /bespin/i
-      })).toBeInTheDocument()
-  })
-
-  it('Testando filtro "maior que"', async () => {
-    render(
-        <AppProvider>
-            <App />
-        </AppProvider>
-      )
-
-      const getValue = screen.getByRole("spinbutton");
-      const getButton = screen.getByRole('button', {
-        name: 'Filtrar'
-      })
-      const column = screen.getByTestId('column-filter')
-      const comparison = screen.getByTestId('comparison-filter')
-
-      userEvent.selectOptions(column, 'diameter')
-      userEvent.selectOptions(comparison, 'maior que')
-      userEvent.type(getValue, '10465')
-      userEvent.click(getButton)
-  })
-
-  it('Testando filtro "igual a"', async () => {
-    render(
-        <AppProvider>
-            <App />
-        </AppProvider>
-      )
-
-      const getValue = screen.getByRole("spinbutton");
-      const getButton = screen.getByRole('button', {
-        name: 'Filtrar'
-      })
-      const column = screen.getByTestId('column-filter')
-      const comparison = screen.getByTestId('comparison-filter')
-
-      userEvent.selectOptions(column, 'surface_water')
-      userEvent.selectOptions(comparison, 'igual a')
-      userEvent.type(getValue, '1')
-      userEvent.click(getButton)
-
+      expect(screen.getByTestId('comparison-filter')).toBeInTheDocument()
+      expect(screen.getByTestId('value-filter')).toBeInTheDocument()
+      expect(screen.getByTestId('button-filter')).toBeInTheDocument()
+      expect(screen.getByTestId('button-remove-filters')).toBeInTheDocument()
+      expect(screen.getByTestId('column-sort-input-asc')).toBeInTheDocument()
+      expect(screen.getByTestId('column-sort-input-desc')).toBeInTheDocument()
+      expect(screen.getByTestId('column-sort-button')).toBeInTheDocument()
       expect(await screen.findByRole('cell', {
         name: /tatooine/i
       })).toBeInTheDocument()
+      
+ })
 
-      const getDeletBtn = screen.getByRole('button', {
-        name: /excluir/i
-      })
 
-      const surface = screen.getByTestId('column-filter')
+ it('Testando o componente "Filter"', async () => {
+  render(
+    <AppProvider>
+        <App />
+    </AppProvider>
+  )
 
-      expect(surface).toBeInTheDocument()
+    const filterName = screen.getByTestId('name-filter')
+    const selectColumn = screen.getByTestId('column-filter')
+    const comparisonFilter = screen.getByTestId('comparison-filter')
+    const valueFilter = screen.getByTestId('value-filter')
+    const filterButton = screen.getByTestId('button-filter')
 
-      userEvent.click(getDeletBtn)
+    userEvent.type(filterName, 'Tatooine')
 
-  })
+    expect(await screen.findByRole('cell', {
+      name: /tatooine/i
+    })).toBeInTheDocument()
 
-  it('Testando botao de limpar filtros e excluir filtros', async () => {
-    render(
-      <AppProvider>
-          <App />
-      </AppProvider>
-    )
+    userEvent.clear(filterName)
+    userEvent.selectOptions(selectColumn, 'rotation_period')
+    userEvent.selectOptions(comparisonFilter, 'igual a')
+    userEvent.type(valueFilter, '24')
 
-    const getValue = screen.getByRole("spinbutton");
-    const getButton = screen.getByRole('button', {
-      name: 'Filtrar'
+    act(() => {
+      userEvent.click(filterButton)
     })
-    const column = screen.getByTestId('column-filter')
-    const comparison = screen.getByTestId('comparison-filter')
 
 
-    userEvent.type(getValue, '1')
-    userEvent.click(getButton)
-
-    const deletBtn = screen.getByRole('button', {
+    const deletButton = screen.getByRole('button', {
       name: /excluir/i
     })
 
-    userEvent.click(deletBtn)
+    expect(screen.getAllByTestId('planet-name')).toHaveLength(3)
+    expect(screen.getByTestId('filter'))
+    expect(deletButton).toBeInTheDocument()
 
-    expect(deletBtn).not.toBeInTheDocument()
+    userEvent.click(deletButton)
 
-    userEvent.type(getValue, '100')
-    userEvent.click(getButton)
+    expect(screen.getAllByTestId('planet-name')).toHaveLength(10)
 
-    const clearFilters = screen.getByRole('button', {
-      name: /remover filtros/i
+    userEvent.selectOptions(selectColumn, 'rotation_period')
+    userEvent.selectOptions(comparisonFilter, 'maior que')
+    userEvent.type(valueFilter, '24')
+
+    act(() => {
+      userEvent.click(filterButton)
     })
 
-    userEvent.click(clearFilters)
+    userEvent.click(deletButton)
 
-    expect(deletBtn).not.toBeInTheDocument()
+    userEvent.selectOptions(selectColumn, 'diameter')
+    userEvent.selectOptions(comparisonFilter, 'menor que')
+    userEvent.type(valueFilter, '10000')
 
-    userEvent.selectOptions(column, 'surface_water')
-    userEvent.selectOptions(comparison, 'menor que')
-    userEvent.type(getValue, '40')
-    userEvent.click(getButton)
+    act(() => {
+      userEvent.click(filterButton)
+    })
 
-    userEvent.selectOptions(column, 'rotation_period')
-    userEvent.selectOptions(comparison, 'maior que')
-    userEvent.type(getValue, '23')
-    userEvent.click(getButton)
-
-    expect(await screen.findByRole('cell', {
-      name: /naboo/i
-    })).toBeInTheDocument()
-
-    userEvent.click(clearFilters)
-
-    userEvent.selectOptions(column, 'rotation_period')
-    userEvent.selectOptions(comparison, 'igual a')
-    userEvent.type(getValue, '23')
-    userEvent.click(getButton)
-
-    const getDelet = screen.getByRole('button', {  name: /excluir/i})
-
-    userEvent.click(getDelet)
-
-    userEvent.selectOptions(column, 'rotation_period')
-    userEvent.selectOptions(comparison, 'maior que')
-    userEvent.type(getValue, '23')
-    userEvent.click(getButton)
-
-    userEvent.click(getDelet)
-    userEvent.click(clearFilters)
-
-    userEvent.selectOptions(column, 'rotation_period')
-    userEvent.selectOptions(comparison, 'menor que')
-    userEvent.type(getValue, '23')
-    userEvent.click(getButton)
-
-    userEvent.click(getDelet)
-  })
-
-  it('Testes no componente "filterTable"', async () => {
-    render(
-      <AppProvider>
-          <App />
-      </AppProvider>
-    )
-
-      const radioBtnAsc = screen.getByRole('radio', {
-        name: /ascendente/i
-      })
-
-      const radioBtnDsc = screen.getByRole('radio', {
-        name: /decrescente/i
-      })
-
-      const filterBtn = screen.getByTestId('column-sort-button')
-
-      expect(screen.getByTestId('column-sort')).toBeInTheDocument()
-      expect(radioBtnAsc).toBeInTheDocument()
-      expect(radioBtnDsc).toBeInTheDocument()
-      expect(filterBtn).toBeInTheDocument()
-      expect(screen.getByText(/ascendente/i)).toBeInTheDocument()
-      expect(screen.getByText(/decrescente/i)).toBeInTheDocument()
+    userEvent.click(deletButton)
 
 
-      userEvent.click(radioBtnAsc)
-      userEvent.click(filterBtn)
+    userEvent.selectOptions(selectColumn, 'surface_water')
+    userEvent.selectOptions(comparisonFilter, 'menor que')
+    userEvent.type(valueFilter, '10000')
+    userEvent.selectOptions(selectColumn, 'population')
+    userEvent.selectOptions(comparisonFilter, 'igual a')
+    userEvent.type(valueFilter, '30000000')
 
-      userEvent.click(radioBtnDsc)
-      userEvent.click(filterBtn)
+    expect(screen.getAllByTestId('filter').length).toBe(2)
 
-  })
+    const deletAllFilters = screen.getByTestId('button-remove-filters')
+
+    userEvent.click(deletAllFilters)
+ })
+
+ it('Testes no componente "FiltersTable"', async () => {
+  render(
+    <AppProvider>
+        <App />
+    </AppProvider>
+  )
+
+  const filterTable = screen.getByTestId('column-sort')
+  const radioAsc = screen.getByTestId('column-sort-input-asc')
+  const radioDec = screen.getByTestId('column-sort-input-desc')
+  const filterTableBtn = screen.getByTestId('column-sort-button')
+
+  userEvent.selectOptions(filterTable, 'population')
+  userEvent.click(radioAsc)
+
+    act(() => {
+      userEvent.click(filterTableBtn)
+    })
+
+
+  expect(await screen.findByRole('cell', {
+    name: /yavin iv/i
+  })).toBeInTheDocument()
+
+  userEvent.click(radioDec)
+  userEvent.click(filterTableBtn)
+
+ })
+
 });
